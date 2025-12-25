c2c9dea31d0ba934c291d0f18851f9ffe3236bf8
user <user@example.com>
2025-12-25 07:48:16 +0530

    Scond push of 25 dec 2025

--- MODULE 4/practice/prac.py
+++ MODULE 4/practice/prac.py
@@ -1,6 +1,6 @@
-import os
+import os 
 import torch 
-from torch import nn
+from torch import nn 
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -9,25 +9,25 @@ print(f'Using {device} device')
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
-        super(NeuralNetwork, self).__init__()
+        super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28*28, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.Linear(512, 10)
         )
-    def forward(self, x):
+    def forward(self,x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
-
+    
 model = NeuralNetwork().to(device)
 print(model)
 
-X = torch.rand(1, 28, 28, device=device)
-logits = model(X)
+x = torch.rand(1,28,28, device=device)
+logits = model(x)
 pred_probab = nn.Softmax(dim=1)(logits)
 y_pred = pred_probab.argmax(1)
 print(f"Predicted class: {y_pred}")
@@ -55,8 +55,6 @@ seq_modules = nn.Sequential(
 )
 input_image = torch.rand(3,28,28)
 logits = seq_modules(input_image)
-print(logits)
 
 softmax = nn.Softmax(dim=1)
 pred_probab = softmax(logits)
-print(pred_probab)
